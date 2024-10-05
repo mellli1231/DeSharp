9cbb5bd6d6706081fad8903fefb7bd857ef559c4
BambooShampoo <tylernguyenvovn@gmail.com>
2024-10-05 15:02:28 -0700

    Gettting Started with Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@ function App() {
   const tasks = useQuery(api.tasks.get);
   return (
     <div className="App">
-      <h1>DeSharp</h1>
-      {tasks?.map(({ _id, text }) => (
-        <div key={_id}>{text}</div>
-      ))}
+      <body>
+        <h1>DeSharp</h1>
+        <section><h3>Report any needles you've spotted in Vancouver and We'll send our folks to clean them up!</h3></section>
+        {tasks?.map(({ _id, text }) => (
+          <div key={_id}>{text}</div>
+        ))}
+      </body>
     </div>
   );
 }
