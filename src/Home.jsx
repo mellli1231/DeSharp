3eba732cc78d53abc2e0bf533311a542bad8794f
Emmanuel de Caiman <emdecaiman@gmail.com>
2024-10-06 04:57:15 -0700

    styled form

--- src/Home.jsx
+++ src/Home.jsx
@@ -70,11 +70,11 @@ function Home() {
       </section>
 
     <section>
-
         <h3>
           Find another needle not on this map?{" "}
         </h3>
       <h3 className="form">Help us DeSharp the city!</h3>
+      <Form></Form>
     </section>
     </>
   );
