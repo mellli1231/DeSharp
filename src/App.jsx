3aaaf37a113a1681e058e1c9436a1674c8aab8ab
Emmanuel de Caiman <emdecaiman@gmail.com>
2024-10-05 17:27:00 -0700

    form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import "./App.css";
+import Form from "./components/Form.jsx"
 import { useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
 
 function App() {
   const tasks = useQuery(api.tasks.get);
   return (
-    <div className="App">
-      {tasks?.map(({ _id, text }) => (
-        <div key={_id}>{text}</div>
-      ))}
-    </div>
+    <>
+      <Form />
+    </>
   );
 }
 
