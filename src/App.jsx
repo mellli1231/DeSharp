c9e61a925c29e41a1a554d92e41a039792dc950c
BambooShampoo <tylernguyenvovn@gmail.com>
2024-10-05 19:20:06 -0700

    Initial Landing page done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Form from "./components/Form.jsx"
 import { useQuery, useMutation } from "convex/react";
 import { api } from "../convex/_generated/api";
-import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from "@vis.gl/react-google-maps";
+import { APIProvider, Map, AdvancedMarker, InfoWindow } from "@vis.gl/react-google-maps";
 import { useState } from "react";
+import { Link } from "react-router-dom"; // Import Link for navigation
 
 function App() {
   const tasks = useQuery(api.tasks.get);
-  const position = { lat: 49.256104, lng: -123.11355 };
+  const position = { lat: 49.282756, lng: -123.120774};
+  const position2 = { lat: 49.282142, lng: -123.121016 };
   const [open, setOpen] = useState(false);
 
   // Access environment variables using import.meta.env
@@ -15,6 +17,12 @@ function App() {
   const googleMapsId = import.meta.env.VITE_GOOGLE_MAPS_ID;
 
   const addReport = useMutation(api.mutate.createTask);
+
+  const [viewState, setViewState] = useState({
+    longitude: -123,
+    latitude: 49,
+    zoom: 13.5
+  });
   
   return (
 
@@ -27,22 +35,40 @@ function App() {
         </h3>
       </header>
 
-      <APIProvider apiKey={googleMapsApiKey}>
-        <div style={{ height: "100vh", width: "100%" }}>
-          <Map zoom={9} center={position} mapId={googleMapsId}>
-            <AdvancedMarker position={position} onClick={() => setOpen(true)}>
-              <Pin />
-            </AdvancedMarker>
-            {open && (
-              <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
-                <p>Test</p>
-              </InfoWindow>
-            )}
-          </Map>
-        </div>
+      <section className="form-link">
+        <h2>
+          Find another needle not on this map?{" "}
+          
+        </h2>
+      </section>
 
-        {/* Update the link to use React Router or appropriate navigation */}
-      </APIProvider>
+      <section className="map-container">
+        <APIProvider apiKey={googleMapsApiKey}>
+          <div style={{height: "90vh", width: "60%" }}>
+            <Map
+              defaultCenter={position}
+              defaultZoom={13}
+              mapId={googleMapsId}
+              {...viewState}
+              onMove={evt => setViewState(evt.viewState)}
+              onZoomChanged={evt => setViewState(evt.viewState)}
+              >
+              <AdvancedMarker position={position} onClick={() => setOpen(true)}>
+              </AdvancedMarker>
+              
+              <AdvancedMarker position={position2} onClick={() => setOpen(true)}>
+              </AdvancedMarker>
+
+              {open && (
+                <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
+                  <p>Test</p>
+                </InfoWindow>
+              )}
+            </Map>
+          </div>
+        </APIProvider>
+      </section>
+
 
       {/* Add loading and error states for better UX */}
       {tasks ? (
